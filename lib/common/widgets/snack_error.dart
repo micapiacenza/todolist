dea5a45c97567f57ce751568d2d186d66f873f8d
micapiacenza <micapiacenza219@gmail.com>
2023-01-13 17:22:01 +1000

    Adding snack error widget

---
+++ lib/common/widgets/snack_error.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/common/theme/theme.dart';
+
+SnackBar SnackError({required String message}) {
+  return SnackBar(
+    backgroundColor: Colors.red[200],
+    content: Text(message,
+      style: lightTheme.textTheme.bodyText1,
+      textAlign: TextAlign.center,
+    ),
+  );
+}
