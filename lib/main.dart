ea0890fc45339c9dbe4257be66553d5ad5316b6c
micapiacenza <micapiacenza219@gmail.com>
2023-01-13 17:22:47 +1000

    Hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todolist/root.dart';
 
 import 'common/theme/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(ProviderScope(child: MyApp()));
 }
 
